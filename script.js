7d2a21239a55c7d96d067cc353f89b280af6399a
3BTUNAIJI <86256795+3BTUNAIJI@users.noreply.github.com>
2025-04-30 06:08:42 +0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Welcome to Pure Matcha - Premium Japanese Tea.");
-    alert("Welcome to Pure Matcha! Discover the taste of tradition.");
-});
