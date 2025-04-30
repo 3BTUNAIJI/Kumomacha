68bbd02b3900907f5ec46ef5d775f3e96afb5b26
3BTUNAIJI <86256795+3BTUNAIJI@users.noreply.github.com>
2025-04-30 05:03:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to Pure Matcha - Premium Japanese Tea.");
+    alert("Welcome to Pure Matcha! Discover the taste of tradition.");
+});
